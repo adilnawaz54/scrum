64383d0121993a55c3a5bb10f569dfa382eec60f
adilnawaz54 <adilnawaz54@gmail.com>
2025-06-20 10:24:46 +0530

    Update codecompilation.groovy

--- sprint-4/my_shared_library/src/org/p14/ansibleCI/codecompilation.groovy
+++ sprint-4/my_shared_library/src/org/p14/ansibleCI/codecompilation.groovy
@@ -1,1 +1,19 @@
+package org.p14.ansibleCI
 
+def call(String buildDirectory = '.') {
+    stage("React: Install Dependencies") {
+        script {
+            dir(buildDirectory) {
+                sh 'npm install'
+            }
+        }
+    }
+
+    stage("React: Code Compilation") {
+        script {
+            dir(buildDirectory) {
+                sh 'npm run build'
+            }
+        }
+    }
+}
