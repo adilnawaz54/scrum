daebfd849227eeab9c9027b7bc584efaf0e44f61
adilnawaz54 <adilnawaz54@gmail.com>
2025-07-02 03:10:24 +0530

    Create DSL(redis-role).groovy

---
+++ sprint-4/DSL/scylla-CI-CD/CI/scyllaDB-CI/DSL(redis-role).groovy
@@ -0,0 +1,63 @@
+folder('redis-CI-CD') { 
+    displayName('redis-CI-CD') 
+    description('Contains redisDB CI-CD')
+}
+
+folder('redis-CI-CD/CI') { 
+    displayName('CI') 
+    description('CI deployments')
+}
+
+
+
+// Pipeline job for CI-redis
+pipelineJob("redis-CI-CD/CI/redis-role") {
+    description("Deploys/Destroys Deploy-instance using redis-CI-CD.")
+    
+    parameters {
+        stringParam('BRANCH_NAME', 'SCRUMS-462-Adil', 'Git branch to use')
+       
+    }
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/snaatak-Downtime-Crew/jenkins-pipeline.git')
+                        credentials('downtime_github')
+                    }
+                   branches('*/${BRANCH_NAME}')
+                }
+            }
+            scriptPath("redis-CI/role/Jenkinsfile")
+        }
+    }
+}
+println "✔︎ Pipeline job created → redis-CI-CD/CI/redis-playbook"
+
+// Pipeline job for CI-redis
+pipelineJob("redis-CI-CD/CI/redis-playbook") {
+    description("Deploys/Destroys Deploy-instance using redis-CI-CD.")
+    
+    parameters {
+        stringParam('BRANCH_NAME', 'SCRUMS-464-Adil', 'Git branch to use')
+       
+    }
+    
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url('https://github.com/snaatak-Downtime-Crew/jenkins-pipeline.git')
+                        credentials('downtime_github')
+                    }
+                   branches('*/${BRANCH_NAME}')
+                }
+            }
+            scriptPath("redis-CI/playbook/Jenkinsfile")
+        }
+    }
+}
+println "✔︎ Pipeline job created → redis-CI-CD/CI/redis-playbook"
