9da2ae493a4d7859f9e50c8e9e9d8d39b94836f9
adilnawaz54 <adilnawaz54@gmail.com>
2025-07-02 17:59:35 +0530

    Update DSL(redis-role).groovy

--- sprint-4/DSL/scylla-CI-CD/CI/scyllaDB-CI/DSL(redis-role).groovy
+++ sprint-4/DSL/scylla-CI-CD/CI/scyllaDB-CI/DSL(redis-role).groovy
@@ -30,34 +30,9 @@ pipelineJob("redis-CI-CD/CI/redis-role") {
                    branches('*/${BRANCH_NAME}')
                 }
             }
-            scriptPath("redis-CI/role/Jenkinsfile")
+            scriptPath("Redis-CI/Role/Jenkinsfile")
         }
     }
 }
-println "✔︎ Pipeline job created → redis-CI-CD/CI/redis-playbook"
+println "✔︎ Pipeline job created → redis-CI-CD/CI/redis-role"
 
-// Pipeline job for CI-redis
-pipelineJob("redis-CI-CD/CI/redis-playbook") {
-    description("Deploys/Destroys Deploy-instance using redis-CI-CD.")
-    
-    parameters {
-        stringParam('BRANCH_NAME', 'SCRUMS-464-Adil', 'Git branch to use')
-       
-    }
-    
-    definition {
-        cpsScm {
-            scm {
-                git {
-                    remote {
-                        url('https://github.com/snaatak-Downtime-Crew/jenkins-pipeline.git')
-                        credentials('downtime_github')
-                    }
-                   branches('*/${BRANCH_NAME}')
-                }
-            }
-            scriptPath("redis-CI/playbook/Jenkinsfile")
-        }
-    }
-}
-println "✔︎ Pipeline job created → redis-CI-CD/CI/redis-playbook"
